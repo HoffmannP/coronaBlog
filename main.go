c214e3533978dfde45448f26805819068c3dc0f6
Berengar W. Lehr <Berengar.Lehr@kompetenztest.de>
2020-03-09 15:28:12 +0100

    Really send signal

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os/exec"
 	"strings"
 )
 
 var fromUser string
 var toUser string
+var saveFile string
 
 func textize(input string) (output string) {
 	output = strings.ReplaceAll(input, "<strong>", "")
@@ -15,16 +17,16 @@ func textize(input string) (output string) {
 }
 
 func sendSignal(format string, a ...interface{}) {
-	// err := exec.Command("/usr/local/bin/signal-cli", "-u", fromUser, "send", "-m", fmt.Sprintf(format, a...), toUser).Run()
-	_, err := fmt.Println("/usr/local/bin/signal-cli", "-u", fromUser, "send", "-m", fmt.Sprintf(format, a...), toUser)
+	err := exec.Command("/usr/local/bin/signal-cli", "-u", fromUser, "send", "-m", fmt.Sprintf(format, a...), toUser).Run()
+	// _, err := fmt.Println("/usr/local/bin/signal-cli", "-u", fromUser, "send", "-m", fmt.Sprintf(format, a...), toUser)
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
 func main() {
-	setSignalUsers()
-	s := load("/home/ber/bin/corona")
+	setConfig()
+	s := load(saveFile)
 	stadtJena(&s.Stadt)
 	otzBlogThueringen(&s.OtzThueringen)
 	otzBlogWeltweit(&s.OtzWeltweit)
